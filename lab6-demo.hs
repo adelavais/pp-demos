5e50e3597ef86f9b98c0fa2f571d494a27b8e6ce
Adela Vais <adela.vais99@gmail.com>
2021-04-12 02:42:22 +0300

    use spaces instead of tabs in lab6 demo

--- lab6-demo.hs
+++ lab6-demo.hs
@@ -10,8 +10,8 @@ f2 x y = x + y
 -- https://stackoverflow.com/questions/15183324/what-is-the-difference-between-pure-and-impure-in-haskell
 
 main = do
-	print (f1 2 3) -- 5
-	print (f2 2 3) -- 5
+    print (f1 2 3) -- 5
+    print (f2 2 3) -- 5
 
 -- Moduri de a scrie apelurile de functii
 
@@ -25,69 +25,69 @@ main = do
 
 (2 +) == \x -> 2 + x
 (+ 2) == \x -> x + 2
-(- 2) == -2          -- minus e considerat semn 
+(- 2) == -2        -- minus e considerat semn
 (2 -) == \x -> 2 - x
 
 -}
 
 -- Listele sunt omogene (toate elementele au acelasi tip)
 -- Diverse moduri de a genera liste [1,3,5]
-	print $ [1, 3, 5]
-	print $ 1 : 3 : 5 : []
-	print $ [1, 3 .. 5]
-	print $ [1, 3 .. 6]
-	
+    print $ [1, 3, 5]
+    print $ 1 : 3 : 5 : []
+    print $ [1, 3 .. 5]
+    print $ [1, 3 .. 6]
+
 -- : operatorul cons
 -- ++ operatorul de concatenare
-	print $ 1 : [2,3]
-	print $ [1] ++ [2,3]
+    print $ 1 : [2,3]
+    print $ [1] ++ [2,3]
 
 -- head si tail sunt echivalantele pentru car si cdr
-	print $ head [1, 2, 3] -- 1
-	print $ tail [1, 2, 3] -- [2, 3]
+    print $ head [1, 2, 3] -- 1
+    print $ tail [1, 2, 3] -- [2, 3]
 
 -- in plus, exista last si init
-	print $ last [1, 2, 3] -- 3
-	print $ init [1, 2, 3] -- [1, 2]
+    print $ last [1, 2, 3] -- 3
+    print $ init [1, 2, 3] -- [1, 2]
 
--- vreau sa imi creez un predicat care sa imi faca 3 verificari: 
+-- vreau sa imi creez un predicat care sa imi faca 3 verificari:
 -- lista sa nu fie nula, 3 sa fie element al acesteia si 5 sa nu fie
 -- mypred L = not null L &&  elem 3 L && notElem 5 L
-	print $ mypred [1,2,3,4,5]
+    print $ mypred [1,2,3,4,5]
 
 -- Tupluri -> sintaxa cu paranteze rotunde (la liste este cu paranteze patrate)
-	print $ ("Hello", True, 2)
-	print $ fst ("Hello", True)  -- "Hello"
-	print $ snd ("Hello", True)  -- True
+    print $ ("Hello", True, 2)
+    print $ fst ("Hello", True)  -- "Hello"
+    print $ snd ("Hello", True)  -- True
 
 -- Functionale
-	print $ map (+ 2) [1, 2, 3]                -- [3, 4, 5]
-	print $ filter odd [1, 2, 3, 4]            -- [1, 3]
-	print $ foldl (+) 0 [1, 2, 3, 4]           -- 10
-	print $ foldl (-) 0 [1, 2]                 -- -3 <==> (0 - 1) - 2
-	print $ foldr (-) 0 [1, 2]                 -- -1 <==> 1 - (2 - 0)
-	print $ zip [1, 2] [3, 4]                  -- [(1, 3), (2, 4)]
-	print $ zipWith (+) [1, 2] [3, 4]          -- [4, 6]
+    print $ map (+ 2) [1, 2, 3]             -- [3, 4, 5]
+    print $ filter odd [1, 2, 3, 4]          -- [1, 3]
+    print $ foldl (+) 0 [1, 2, 3, 4]         -- 10
+    print $ foldl (-) 0 [1, 2]              -- -3 <==> (0 - 1) - 2
+    print $ foldr (-) 0 [1, 2]              -- -1 <==> 1 - (2 - 0)
+    print $ zip [1, 2] [3, 4]               -- [(1, 3), (2, 4)]
+    print $ zipWith (+) [1, 2] [3, 4]        -- [4, 6]
 
--- Functia primita de foldl si foldr are o ordine a parametrilor diferita 
-	print $ foldr (\e acc -> e : acc) [] [1,2,3,4] -- [1,2,3,4]
-	print $ foldl (\acc e -> e : acc) [] [1,2,3,4] -- [4,3,2,1]
-	
+-- Functia primita de foldl si foldr are o ordine a parametrilor diferita
+    print $ foldr (\e acc -> e : acc) [] [1,2,3,4] -- [1,2,3,4]
+    print $ foldl (\acc e -> e : acc) [] [1,2,3,4] -- [4,3,2,1]
+
 -- List comprehension
-	print $ take 5 [x | x <- [0, 2 ..], x `mod` 3 == 0] -- lista numerelor naturale pare, divizibile cu 3
+    print $ take 5 [x | x <- [0, 2 ..], x `mod` 3 == 0] -- lista numerelor naturale pare, divizibile cu 3
 -- lista completa ar fi [0,6,12,18,24,30,36,42,48,54,60,66,72,78,84,90,96,102,108, ...
 
 -- Garzi
 -- Case
 -- Pattern matching
-	print $ factorial_if 5
-	print $ factorial_guards 5
-	print $ factorial_case 5
-	print $ factorial_pm 5
-	print $ factorial_pm_tail 5 1
+    print $ factorial_if 5
+    print $ factorial_guards 5
+    print $ factorial_case 5
+    print $ factorial_pm 5
+    print $ factorial_pm_tail 5 1
 
 -- Point free functions
-	print $ my_point_free_func 5 -- 26
+    print $ my_point_free_func 5 -- 26
 
 --------------------------------------------------------------------
 mypred l = not (null l) &&  elem 3 l && notElem 5 l
